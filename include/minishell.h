6f9c7ca7e6166964365ab97319825dfa88fd61e8
zcanales <zaloacanales@gmail.com>
2021-12-27 17:31:58 +0100

    init-2.2

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: zcanales <zcanales@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 15:50:40 by zcanales          #+#    #+#             */
-/*   Updated: 2021/12/27 14:15:41 by zcanales         ###   ########.fr       */
+/*   Updated: 2021/12/27 17:30:03 by zcanales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,17 @@ typedef struct s_pro
 {
 	int 	nprocess;
     int     *pid;
-    int     **pipe;
+    int     **fd;
     char    **commands;
+
 }   t_pro;
 
 typedef struct s_env
 {
     char    **env;
     char    **paths;
-    char    *home;
-    char    *root;
+ //   char    *home;
+   // char    *root;
 }t_env;
 
 typedef struct	s_shell
@@ -50,12 +51,12 @@ void get_environment(t_env *my_env, char **env);
 
 /* TERMINAL	*/
 int		create_terminal(t_shell *shell);
-//void	rl_clear_history(void);
 void    attributes();
 
 /*PROCESSES*/
 int input(t_shell *shell);
 void exe_command(char *order, t_pro *proc, t_env *env);
-void alloc_process(t_pro *proc);
+void alloc_process(t_shell *shell);
 
+/*	BUILTINS	*/
 #endif
